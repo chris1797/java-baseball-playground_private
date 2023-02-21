5ee647b5946810802cf0aa8dd1255ee318637ce4
chris1797 <jaehun6004@gmail.com>
2023-02-22 01:41:40 +0900

    문자열계산기 구현

--- src/main/java/StringCalculator/Calculator.java
+++ src/main/java/StringCalculator/Calculator.java
@@ -1,7 +1,11 @@
 package StringCalculator;
 
 
+import java.util.List;
+
 public class Calculator {
+
+    StringParsing stringParsing;
 
 
     public int add(int a, int b) {
@@ -20,10 +24,39 @@ public class Calculator {
         try {
             return a/b;
         } catch (Exception e) {
-            System.out.println("첫번째 값을 두번째 값으로 나눌 수 없습니다.");
+            System.out.println("값을 나눌 수 없습니다.");
         }
 
         return 0;
+    }
+
+    public int calculate(int i, char s, int j) {
+        int num = 0;
+        switch (s) {
+            case '+':
+                num = add(i,j);
+            case '-':
+                num = subtract(i,j);
+            case '*':
+                num = multiple(i,j);
+            case '/':
+                num = divide(i,j);
+        }
+        return num;
+    }
+
+
+    public int calculateString(String str) {
+        stringParsing = new StringParsing();
+
+        String[] strList = stringParsing.seperate(str);
+        int num = stringParsing.parsingToInt(strList[0]);
+
+        for(int i=0; i<strList.length-2; i+=2){
+            num = calculate(num, strList[i+1].charAt(0), stringParsing.parsingToInt(strList[i+2]));
+        }
+
+        return num;
     }
 
 
--- src/main/java/StringCalculator/StringParsing.java
+++ src/main/java/StringCalculator/StringParsing.java
@@ -1,4 +1,17 @@
 package StringCalculator;
 
+import java.util.List;
+
 public class StringParsing {
+
+    public String[] seperate(String value) {
+        return value.split(" ");
+    }
+
+    public int parsingToInt(String value) {
+        return Integer.parseInt(value);
+    }
+
+
+
 }
