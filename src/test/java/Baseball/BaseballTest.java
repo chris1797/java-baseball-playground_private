019b4fdf4fb1a76b2a4017fffa50b972b5d538df
chris1797 <jaehun6004@gmail.com>
2023-03-14 23:29:58 +0900

    createRandomNum, parsingToIntArray 구현

--- src/test/java/Baseball/BaseballTest.java
+++ src/test/java/Baseball/BaseballTest.java
@@ -3,7 +3,12 @@ package Baseball;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BaseballTest {
@@ -19,13 +24,19 @@ class BaseballTest {
     @DisplayName("랜덤 숫자 생성 테스트")
     void createRandomNum() {
         int[] ranNums = baseball.createRandomNum();
+        assertEquals(3, ranNums.length);
     }
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
     @DisplayName("입력된 숫자 파싱 테스트")
     void inputNums() throws Exception {
         String str = "123";
-        int[] inputNums = baseball.inputNums(str);
+        int[] inputNums = baseball.parsingToIntArray(str);
+        /*
+        baseball.parsingToIntArray(str)의 값이 ints와 같은지 테스트
+         */
+        assertThat(Arrays.stream(inputNums).anyMatch(INT_STREAM::equals));
     }
 
 
